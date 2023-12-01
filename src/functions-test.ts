56cea88553a5d03e371ed02f7e5ccde6c5c5250a
Conner Aiken <conner.aiken@ironhealth.io>
2023-12-01 12:39:11 -0800

    Rest example

--- src/functions-test.ts
+++ src/functions-test.ts
@@ -1,5 +1,10 @@
-import addNumbers, { addStrings } from "./functions";
+import addNumbers, { addStrings, formatThings, fetchData, introduce } from "./functions";
 
 console.log(addNumbers(1, 2));
 console.log(addStrings('123', 'abc'));
+console.log(formatThings('Hello World', 1));
+console.log(formatThings('Hello World', 'Test'));
+console.log(fetchData('https://ironhealth.io/get'));
+console.log(introduce('Howdy', ...['Jack', 'John', 'Conner']));
+
 // console.log(addNumbers(1, "jack")); 
--- src/functions.ts
+++ src/functions.ts
@@ -7,4 +7,14 @@ export const addStrings = (str1: string, str2: string): string => {
     return str1 + ' ' + str2;
 }
  
+export const formatThings = (title: string, param: string | number): string => {
+    return title + ': ' + param;
+}
+
+export const fetchData = (url: string): Promise<string> => Promise.resolve('Data from '+url);
+
+export function introduce(salutation: string, ...names: string[] ) : string {
+    return `${salutation} ${names.join(" ")}`;
+}
+
 export default addNumbers;
