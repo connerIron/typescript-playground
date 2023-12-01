ca8e592aca2c2cf201205a31e84cb233845db935
Conner Aiken <conner.aiken@ironhealth.io>
2023-12-01 12:30:08 -0800

    Init to win it

---
+++ src/functions.ts
@@ -0,0 +1,10 @@
+
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+export const addStrings = (str1: string, str2: string): string => {
+    return str1 + ' ' + str2;
+}
+ 
+export default addNumbers;
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+let isLoggedIn: boolean = true;
+
+
+let myRegex: RegExp = /foo/;
+
+const names: string[] = [];
+const namesB: Array<String> = [];
+
+names.push('Hello');
+namesB.push('World');
+
+interface Person {
+    first: string,
+    last: string;
+}
+const myPerson: Person = {
+    first: "Conner",
+    last: "Aiken" 
+};
+
+const mappedIds: Record<number,string> = {
+    100: "Affordable",
+    150: "Pricey",
+    200: "Extravagent"
+};
+
